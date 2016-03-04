f6d06cd5694870695cd3af4cd091251010b01656
mark jervelund <mark@jervelund.com>
2016-03-04 14:07:41 +0100

    Added all the functions

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -14,15 +14,18 @@ public class HeapSort {
     4 A:heap-size D A:heap-size  1
     5 MAX-HEAPIFY.A; 1/
      */
+    private void heapbuild(){
+
+    }
     /*
     MAX-HEAP-INSERT.A; key/
     1 A:heap-size D A:heap-size C 1
     2 AOEA:heap-size D 1
     3 HEAP-INCREASE-KEY.A;A:heap-size; key/
-
-
      */
+    private void heapinsert(){
 
+    }
 
 
     /*
@@ -34,6 +37,51 @@ public class HeapSort {
     5 exchange AOEi with AOEPARENT.i /
     6 i D PARENT.i /
      */
+    private void heapinsertkey(){
+
+    }
+    /*
+    HEAP-INCREASE-KEY.A; i; key/
+    1 if key < AOEi
+    2 error “new key is smaller than current key”
+    3 AOEi D key
+    4 while i > 1 and AOEPARENT.i / < AOEi
+    5 exchange AOEi with AOEPARENT.i /
+    6 i D PARENT.i /
+     */
+    private void heapinceasekey(){
+
+    }
+    /*
+    MAX-HEAP-INSERT.A; key/
+    1 A:heap-size D A:heap-size C 1
+    2 AOEA:heap-size D 1
+    3 HEAP-INCREASE-KEY.A;A:heap-size; key/
+     */
+    private void maxheapinsert(){
+
+    }
+    /*
+    HEAP-MAXIMUM.A/
+    1 return AOE1
+    */
+    private void heapmax(){
+
+    }
+
+    /*
+    HEAP-EXTRACT-MAX.A/
+    1 if A:heap-size < 1
+    2 error “heap underflow”
+    3 max D AOE1
+    4 AOE1 D AOEA:heap-size
+    5 A:heap-size D A:heap-size  1
+    6 MAX-HEAPIFY.A; 1/
+    7 return max
+     */
+    private void heapextractmax(){
+
+    }
 
 
 }
