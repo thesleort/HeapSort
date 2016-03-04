22d4dcd3c4177565eaa6984c2139de18f77d4e21
Troels Blicher Petersen <troels@newtec.dk>
2016-03-04 14:56:50 +0100

    Added interface and interface class

---
+++ src/Element.java
@@ -0,0 +1,11 @@
+/**
+ * Created by troels on 3/4/16.
+ */
+public class Element {
+    public int key;
+    public Object data;
+    public Element (int i, Object o) {
+        this.key = i;
+        this.data = o;
+    }
+}
