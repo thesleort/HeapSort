67ae4e343645ed74cec331b13193fc7f645bc719
mark jervelund <mark@jervelund.com>
2016-03-04 14:01:00 +0100

    Added comments to heapsort.java

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -4,4 +4,36 @@ public class HeapSort {
     public static void main(String[] args) {
         System.out.println("CR4P");
     }
+
+
+    /*
+    HEAPSORT.A/
+    1 BUILD-MAX-HEAP.A/
+    2 for i D A:length downto 2
+    3 exchange AOE1 with AOEi
+    4 A:heap-size D A:heap-size  1
+    5 MAX-HEAPIFY.A; 1/
+     */
+    /*
+    MAX-HEAP-INSERT.A; key/
+    1 A:heap-size D A:heap-size C 1
+    2 AOEA:heap-size D 1
+    3 HEAP-INCREASE-KEY.A;A:heap-size; key/
+
+
+     */
+
+
+
+    /*
+    HEAP-INCREASE-KEY.A; i; key/
+    1 if key < AOEi
+    2 error “new key is smaller than current key”
+    3 AOEi D key
+    4 while i > 1 and AOEPARENT.i / < AOEi
+    5 exchange AOEi with AOEPARENT.i /
+    6 i D PARENT.i /
+     */
+
+
 }
