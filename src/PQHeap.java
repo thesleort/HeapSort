3ae5eb69c0dcfc468c2e19440fc9f93eebd4a6ce
GIT Admin <thesleort@gmail.com>
2016-03-05 21:37:02 +0100

    extractMin() now returns the heap-array

--- src/PQHeap.java
+++ src/PQHeap.java
@@ -61,8 +61,8 @@ public class PQHeap implements EQ {
     }
 
     @Override
-    public Element extractMin() {
-        return null;
+    public Element[] extractMin() {
+        return A;
     }
 
     @Override
