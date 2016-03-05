b60dff60df0d15a5581ea193108e5916af3c9acd
GIT Admin <thesleort@gmail.com>
2016-03-05 21:06:59 +0100

    Converted int-array to Element-array

--- src/PQHeap.java
+++ src/PQHeap.java
@@ -1,65 +1,67 @@
-import javax.lang.model.element.*;
-import javax.lang.model.element.Element;
+
 import java.util.stream.IntStream;
 
 /**
  * Created by Mark on 05-Mar-16.
  */
 public class PQHeap implements EQ {
-    public PQHeap(int maxElms){
-            }
-    private static int[] A;
+    public PQHeap(int maxElms) {
+    }
+
+    private static Element[] A;
     private static int n;
     private static int left;
     private static int right;
     private static int largest;
 
-    private void HeapBuild(int A[]) {
-        n = A.length-1;
+    private void HeapBuild(Element A[]) {
+        n = A.length - 1;
         IntStream.range(A.length, 1).forEach(i -> {
             MinHeapify(A, i);
 
         });
     }
 
-    private void MinHeapify(int[] A, int i) {
+    private void MinHeapify(Element[] A, int i) {
         left = 2 * i;
         right = 2 * i + 1;
-        if (left <= n && A[left] > A[i]){
+        if (left <= n && A[left].key > A[i].key) {
             largest = left;
+        } else {
+            largest = i;
         }
-        else{ largest = i;}
-        if (right <= n && A[right] > A[largest])
+        if (right <= n && A[right].key > A[largest].key)
             largest = right;
-        if (largest != i){
-            Exchange(i,largest);
+        if (largest != i) {
+            Exchange(i, largest);
             MinHeapify(A, largest);
         }
     }
 
     private void Exchange(int i, int largest) {
-        int temp = A[i];
+        Element temp = A[i];
         A[i] = A[largest];
         A[largest] = temp;
     }
 
 
-    private int HeapMin(int A[]) {
-        return A[1];
+    private int HeapMin(Element A[]) {
+        return A[1].key;
     }
-    private void Sort(int[] A) {
+
+    private void Sort(Element[] A) {
         HeapBuild(A);
         IntStream.range(A.length, 0).forEach(i -> {
-            Exchange(A[0],A[i]);
+            Exchange(0, i);
             n--;
-            MinHeapify(A,0);
+            MinHeapify(A, 0);
 
         });
 
     }
 
     @Override
-    public javax.lang.model.element.Element extractMin() {
+    public Element extractMin() {
         return null;
     }
 
