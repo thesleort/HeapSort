95d19503222decbc7435be0a1faba26c3efe7999
GIT Admin <thesleort@gmail.com>
2016-03-09 14:56:16 +0100

    My own implementation of HeapSort from the book

--- src/PQHeap.java
+++ src/PQHeap.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 /**
- * Created by Mark on 05-Mar-16.
+ * Created by Troels Blicher Petersen on 09-Mar-16.
  */
 public class PQHeap implements EQ {
-    //private static Element[] A;
+
     private static ArrayList<Element> A;
     private static int n;
     private static int left;
@@ -17,15 +17,7 @@ public class PQHeap implements EQ {
         A = new ArrayList<>(maxElms);
     }
 
-    private void HeapBuild(ArrayList<Element> A) {
-        n = A.size() - 1;
-        IntStream.range(A.size(), 1).forEach(i -> {
-            MinHeapify(A, i);
-
-        });
-    }
-
-    private void MinHeapify(ArrayList<Element> A, int i) {
+    private void MaxHeapify(ArrayList<Element> A, int i) {
         left = 2 * i;
         right = 2 * i + 1;
         if (left <= n && A.get(left).key > A.get(i).key) {
@@ -33,78 +25,62 @@ public class PQHeap implements EQ {
         } else {
             largest = i;
         }
-        if (right <= n && A.get(right).key > A.get(largest).key)
+        if (right <= n && A.get(right).key > A.get(largest).key) {
             largest = right;
+        }
         if (largest != i) {
-            Exchange(i, largest);
-            MinHeapify(A, largest);
+            Exchange(A.get(i).key, A.get(largest).key);
+            MaxHeapify(A, largest);
         }
     }
 
-    private void Exchange(int i, int largest) {
-        Element temp = A.get(i);
-        A.set(i, A.get(largest));
-        A.set(largest, temp);
+    private void Exchange(int key, int key1) {
+
     }
 
-
-    public int HeapMin(ArrayList<Element> A) {
-        return A.get(1).key;
-    }
-
-    public void Sort() {
-        HeapBuild(A);
-        //    IntStream.range(A.length, 0).forEach(i -> {
-        for (int m = A.size() - 1; m > 0; m--) {
-            Exchange(0, m);
-            n--;
-            MinHeapify(A, 0);
-
+    public int HeapExtractMax(ArrayList<Element> A) throws Exception {
+        if( n < 0) {
+            throw HeapUnderFlowException();
         }
+        int max = A.get(0).key;
+        A.set(0,A.get(A.size()-1));
+        n--;
+        MaxHeapify(A,0);
+        return max;
     }
+
+    private void HeapIncreaseKey(ArrayList<Element> A, int i, Element key) {
+        if (key.key < A.get(i).key) {
+            System.out.println("New key is smaller than current key");
+        }
+        else {
+            A.set(i,new Element(key.key,A.get(i).data));
+            while (i > 0 && A.get(i/2).key < A.get(i).key) {
+                Exchange(A.get(i).key,A.get(i/2).key);
+                i = A.get(i/2).key;
+            }
+        }
+
+    }
+    private void MaxHeapInsert(ArrayList<Element> A, Element key) {
+        n++;
+        A.add(new Element(Integer.MIN_VALUE,null));
+        HeapIncreaseKey(A,n,key);
+    }
+
+    private Exception HeapUnderFlowException() {
+        System.out.println("n is smaller than 0");
+        return null;
+    }
+
 
     @Override
     public Element extractMin() {
-        return A.get(0);
+        return null;
     }
 
-    /**
-     * Inserts an element in the array/heap.
-     *
-     * @param e Element is an object.
-     */
     @Override
     public void insert(Element e) {
-        n++;
-        A.add(e);
-//        A.clone();
-//        A = new Element[temp.length];
-//        for(int i = 0; i < temp.length; i++){
-//            A[i] = temp[i];
-//        }
-//
-//        System.out.println("-----------------------------"+A[0].data);
 
-    }
-
-    public void insertnSort(Element e) {
-        insert(e);
-        Sort();
-    }
-
-    private int HeapExtractMin(ArrayList<Element> A) {
-        if (A.size() < 0) {
-            int min = HeapMin(A);
-            A.set(0, A.get(A.size()));
-            //A.size() = A.size -1;
-            MinHeapify(A, 1);
-            return min;
-        }
-        return 0;
-    }
-    public ArrayList<Element> getHeap() {
-        return A;
-    }
-    private void heapunderflowException() {
     }
 }
--- src/main.java
+++ src/main.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.PrintStream;
 
 /**
- *      Created by Mark jervelund <Mark@jervelund.com>
- *      & Troels Blicher Petersen <troels@newtec.dk>
+ * Created by Mark jervelund <Mark@jervelund.com>
+ * & Troels Blicher Petersen <troels@newtec.dk>
  */
 public class main {
     public static void main(String[] args) {
@@ -20,23 +20,25 @@ public class main {
         int i = 0;
         if (sc != null) {
             while (sc.hasNext()) {
-                pqHeap.insert(new Element(i, sc.nextInt()));
+                int num = sc.nextInt();
+                pqHeap.insert(new Element(i, num));
                 i++;
             }
         }
         /**
          * While loop shows unsorted array:
          */
-        pqHeap.Sort();
-        for (Element j : pqHeap.getHeap()) {
-            System.out.println(j.data);
+
+        for (int j = 0; j < pqHeap.getHeap().size(); j++) {
+            System.out.println(pqHeap.getHeap().get(j).key + " " + pqHeap.getHeap().get(j).data);
         }
+        pqHeap.Sort();
         System.out.println("----------------------------");
         /**
          * While loop shows sorted array:
          */
         for (Element j : pqHeap.getHeap()) {
-            System.out.println(j.data);
+            System.out.println(j.key + " " + j.data);
         }
     }
 }
