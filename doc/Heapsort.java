cb500b48a989f20c19ca034e59e7f31debb9d97a
Troels Blicher Petersen <troels@newtec.dk>
2016-03-18 15:42:36 +0100

    Added report & small naming fixes

---
+++ doc/Heapsort.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * Created by Mark jervelund          <Mark@jervelund.com>  <Mjerv15>
+ *          & Troels Blicher Petersen <troels@newtec.dk>    <trpet15>
+ */
+public class Heapsort {
+    public static void main(String[] args) {
+
+        /**
+         * Creates a new heap.
+         */
+        PQHeap pqHeap = new PQHeap(0);
+
+        /**
+         * scans incoming data.
+         */
+        Scanner sc = new Scanner(System.in);
+
+        /**
+         * While loop inserts elements in the heap from the scanner.
+         */
+        if (sc != null) {
+            while (sc.hasNext()) {
+                int num = sc.nextInt();
+                pqHeap.insert(new Element(num, null));
+            }
+        }
+
+        /**
+         * Calls the function to sort the Heaptree.
+         */
+        pqHeap.Sort();
+
+        while (true) {
+            try {
+                int key = pqHeap.extractMin().key;
+                System.out.print(key + " ");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
+            }
+        }
+    }
+}
