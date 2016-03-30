566c4ccad31943ff5f436eeb73decd1d903ba0d2
Troels Blicher Petersen <troels@newtec.dk>
2016-03-30 14:06:59 +0200

    Added new main-class

--- src/Heapsort/Heapsort.java
+++ src/Heapsort/Heapsort.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *          & Troels Blicher Petersen <troels@newtec.dk>    <trpet15>
  */
 public class Heapsort {
-    public static void main(String[] args) {
+    public void Heapsort() {
 
         /**
          * Creates a new heap.
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by trpet15 - Troels Blicher Petersen <troels@newtec.dk> on 3/30/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
